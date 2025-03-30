0fbb569411ebe92a523e13b9b23af9f0a05a1969
karthikprabha14 <48215542+karthikprabha14@users.noreply.github.com>
2025-03-30 08:29:25 +0530

    Update Courses.tsx

--- src/Courses.tsx
+++ src/Courses.tsx
@@ -23,9 +23,9 @@ function Courses() {
     useEffect(() => {
         const fetchProducts = async () => {
           try {
-            const response = await fetch('https://fakestoreapi.com/products');
-            const data = await response.json();
-            setCourses(data);
+            // const response = await fetch('https://fakestoreapi.com/products');
+            // const data = await response.json();
+            setCourses([]);
           } catch (error) {
             console.error('Error fetching products:', error);
           } finally {
